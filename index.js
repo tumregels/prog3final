fffdc74fb12702786f5e6d1ab995a2c38f61c711
user <user@lab1553.tumo.lab>
2017-09-18 17:43:27 +0400

    add package.json

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log('Hi');
+const scrapeIt = require("scrape-it");
 
-console.log('Bye');
+// Promise interface
+scrapeIt("http://ionicabizau.net", {
+    title: ".header h1"
+  , desc: ".header h2"
+  , avatar: {
+        selector: ".header img"
+      , attr: "src"
+    }
+}).then(page => {
+    console.log(page);
+});
